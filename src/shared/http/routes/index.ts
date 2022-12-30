bbe023f88ac469325c821c87bf1276ac55aad743
Diego Câmara <diegocmoura@live.com>
2022-12-29 23:36:43 -0300

    Tratamento de erros customizados

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new AppError('Acesso negado', 401)
   return response.json({ message: 'Olá devs' })
 })
 
