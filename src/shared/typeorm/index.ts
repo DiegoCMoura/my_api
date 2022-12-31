2f4b69ed5f5bb42a6e33560edb3b6179fb60948d
Diego Câmara <diegocmoura@live.com>
2022-12-30 23:30:11 -0300

    Entity Role in Entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,10 @@
+import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1672451901934 } from './migrations/1672451901934-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
-  migrations: [],
+  entities: [Role],
+  migrations: [CreateRolesTable1672451901934],
 })
