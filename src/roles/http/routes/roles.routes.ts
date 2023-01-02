6f43d4a4ec91bd3b8f0bc93bc22fe3c8f5b8edcf
Diego Câmara <diegocmoura@live.com>
2023-01-02 16:01:08 -0300

    Finalização do CRUD de Role

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,5 +1,9 @@
 import { createRolesController } from '@roles/useCases/createRole'
+import { deleteRolesController } from '@roles/useCases/deleteRole'
 import { listRolesController } from '@roles/useCases/listRoles'
+import { showRolesController } from '@roles/useCases/showRole'
+import { updateRolesController } from '@roles/useCases/updateRole'
+
 import { Router } from 'express'
 
 const rolesRoute = Router()
@@ -10,6 +14,18 @@ rolesRoute.post('/', (request, response) => {
 
 rolesRoute.get('/', (request, response) => {
   return listRolesController.handle(request, response)
+})
+
+rolesRoute.get('/:id', (request, response) => {
+  return showRolesController.handle(request, response)
+})
+
+rolesRoute.put('/:id', (request, response) => {
+  return updateRolesController.handle(request, response)
+})
+
+rolesRoute.delete('/:id', (request, response) => {
+  return deleteRolesController.handle(request, response)
 })
 
 export { rolesRoute }
