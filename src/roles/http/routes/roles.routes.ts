696226751941b0e21a5086e636962099dbdcc856
Diego Câmara <diegocmoura@live.com>
2023-01-02 16:28:27 -0300

    Validations Roles include

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -5,27 +5,72 @@ import { showRolesController } from '@roles/useCases/showRole'
 import { updateRolesController } from '@roles/useCases/updateRole'
 
 import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 const rolesRoute = Router()
 
-rolesRoute.post('/', (request, response) => {
-  return createRolesController.handle(request, response)
-})
+rolesRoute.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }),
+  }),
+  (request, response) => {
+    return createRolesController.handle(request, response)
+  },
+)
 
-rolesRoute.get('/', (request, response) => {
-  return listRolesController.handle(request, response)
-})
+rolesRoute.get(
+  '/',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+      limit: Joi.number(),
+    }),
+  }),
+  (request, response) => {
+    return listRolesController.handle(request, response)
+  },
+)
 
-rolesRoute.get('/:id', (request, response) => {
-  return showRolesController.handle(request, response)
-})
+rolesRoute.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }),
+  }),
+  (request, response) => {
+    return showRolesController.handle(request, response)
+  },
+)
 
-rolesRoute.put('/:id', (request, response) => {
-  return updateRolesController.handle(request, response)
-})
+rolesRoute.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }),
+  }),
+  (request, response) => {
+    return updateRolesController.handle(request, response)
+  },
+)
 
-rolesRoute.delete('/:id', (request, response) => {
-  return deleteRolesController.handle(request, response)
-})
+rolesRoute.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }),
+  }),
+  (request, response) => {
+    return deleteRolesController.handle(request, response)
+  },
+)
 
 export { rolesRoute }
--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,7 +1,8 @@
-import express, { NextFunction } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import swaggerUI from 'swagger-ui-express'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
 import swaggerFile from '../../swagger.json'
@@ -12,12 +13,10 @@ app.use(express.json())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.use(routes)
+app.use(errors())
 
 app.use(
-  (error: Error,
-  request: Request,
-  response: Response,
-  next: NextFunction) => {
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
         status: 'error',
