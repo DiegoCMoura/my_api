a58f63daf1dea48b255be49092a633bd6537f122
Diego Câmara <diegocmoura@live.com>
2023-01-03 13:11:47 -0300

    Criando entity User

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,17 @@
 import { Role } from '@roles/entities/Role'
+import { User } from '@users/entities/user'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1672451901934 } from './migrations/1672451901934-CreateRolesTable'
+import { CreateUsersTable1672748678003 } from './migrations/1672748678003-CreateUsersTable'
+import { AddRoleIdToUsersTable1672748886775 } from './migrations/1672748886775-AddRoleIdToUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
-  migrations: [CreateRolesTable1672451901934],
+  entities: [Role, User],
+  migrations: [
+    CreateRolesTable1672451901934,
+    CreateUsersTable1672748678003,
+    AddRoleIdToUsersTable1672748886775,
+  ],
 })
