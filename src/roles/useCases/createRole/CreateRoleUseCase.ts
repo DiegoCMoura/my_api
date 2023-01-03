79274074a4c4ec61d139e1d5beaaa398828555c1
Diego Câmara <diegocmoura@live.com>
2023-01-03 08:59:16 -0300

    Criação das injenções de dependencias

--- src/roles/useCases/createRole/CreateRoleUseCase.ts
+++ src/roles/useCases/createRole/CreateRoleUseCase.ts
@@ -1,13 +1,17 @@
 import { Role } from '@roles/entities/Role'
-import { RolesRepository } from '@roles/repositories/RolesRepository'
+import { IRolesRepository } from '@roles/repositories/IRolesRepository'
 import { AppError } from '@shared/errors/AppError'
+import { inject, injectable } from 'tsyringe'
 
 type CreateRoleDto = {
   name: string
 }
 
+@injectable()
 export class CreateRoleUseCase {
-  constructor(private rolesRepository: RolesRepository) {}
+  constructor(
+    @inject('RolesRepository') private rolesRepository: IRolesRepository,
+  ) {}
 
   async execute({ name }: CreateRoleDto): Promise<Role> {
     const roleAlreadyExists = await this.rolesRepository.findByName(name)
