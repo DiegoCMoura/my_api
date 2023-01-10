078a2faef1b266c11cfdc84ac9892b925f75faed
Diego Câmara <diegocmoura@live.com>
2023-01-10 20:11:05 -0300

    Refactor middleware isAuthenticated

---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,29 @@
+import { AppError } from '@shared/errors/AppError'
+import { Request, Response, NextFunction } from 'express'
+import { Secret, verify } from 'jsonwebtoken'
+import authConfig from '@config/auth'
+
+type JwtPayloadProps = {
+  sub: string
+}
+
+export const isAuthenticated = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const authHeader = request.headers.authorization
+  if (!authHeader) {
+    throw new AppError('Failed to verify access token', 401)
+  }
+
+  const token = authHeader.replace('Bearer ', '')
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret as Secret)
+    const { sub } = decodedToken as JwtPayloadProps
+    request.user = { id: sub }
+    return next()
+  } catch {
+    throw new AppError('Invalid authentication token', 401)
+  }
+}
