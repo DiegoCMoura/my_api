ad37313f0e94239b5e4d1fdb3be8c3d283f8748e
Diego Câmara <diegocmoura@live.com>
2023-01-11 21:37:22 -0300

    Create entity and migration RefreshToken

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,17 +1,20 @@
 import { Role } from '@roles/entities/Role'
+import { RefreshToken } from '@users/entities/RefreshToken'
 import { User } from '@users/entities/user'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1672451901934 } from './migrations/1672451901934-CreateRolesTable'
 import { CreateUsersTable1672748678003 } from './migrations/1672748678003-CreateUsersTable'
 import { AddRoleIdToUsersTable1672748886775 } from './migrations/1672748886775-AddRoleIdToUsersTable'
+import { CreateRefreshTokensTable1673483245986 } from './migrations/1673483245986-CreateRefreshTokensTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role, User],
+  entities: [Role, User, RefreshToken],
   migrations: [
     CreateRolesTable1672451901934,
     CreateUsersTable1672748678003,
     AddRoleIdToUsersTable1672748886775,
+    CreateRefreshTokensTable1673483245986,
   ],
 })
