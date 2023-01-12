27bf1c785313d51aae782bc9e9e676ff7817fbab
Diego Câmara <diegocmoura@live.com>
2023-01-11 23:16:13 -0300

    Finalização do curso

--- src/users/http/middlewares/addUserInfoToRequest.ts
+++ src/users/http/middlewares/addUserInfoToRequest.ts
@@ -1,12 +1,11 @@
-import { Request, Response, NextFunction } from 'express'
-import { Secret, verify } from 'jsonwebtoken'
-import authConfig from '@config/auth'
+import { NextFunction, Request, Response } from 'express'
+import { decode } from 'jsonwebtoken'
 
 type JwtPayloadProps = {
   sub: string
 }
 
-export const isAuthenticated = (
+export const addUserInfoToRequest = (
   request: Request,
   response: Response,
   next: NextFunction,
@@ -19,7 +18,6 @@ export const isAuthenticated = (
       message: 'Access token not present',
     })
   }
-
   const token = authHeader.replace('Bearer ', '')
   if (!token) {
     return response.status(401).json({
@@ -28,16 +26,17 @@ export const isAuthenticated = (
       message: 'Access token not present',
     })
   }
-
   try {
-    const decodedToken = verify(token, authConfig.jwt.secret as Secret)
+    const decodedToken = decode(token)
+    console.log(decodedToken)
+
     const { sub } = decodedToken as JwtPayloadProps
     request.user = { id: sub }
     return next()
   } catch {
     return response.status(401).json({
       error: true,
-      code: 'token.expired',
+      code: 'token.invalid',
       message: 'Access token not present',
     })
   }
